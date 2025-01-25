03d176ad5d4f572aed0fc53cc8c12dab34e22e73
snehalkadam04 <156996539+snehalkadam04@users.noreply.github.com>
2025-01-25 18:38:13 +0000

    basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "snehal1223@gmail.com"
+var accountPassword = "12345678"
+accountCity = "mumbai"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("snehal");
+
+// code should be in readable format//
+//ECMAScript language specification 
+
+let name = "snehal"
+let age = 21 
+let isLoggedIn = true 
+
+console.table([name,age,isLoggedIn])
+
+//number => 2 to 53
+//bigint
+//string => ""
+//boolen =>true/false
+//null => standalone value
+//symbol
+
+
+console.log(typeof "snehal");
+
+console.log(typeof null);
+// nul is object
+
+console.log(typeof undefined);
+//undefined
+
+
+
