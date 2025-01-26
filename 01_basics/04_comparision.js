cf4b7f776b55b1b88b2b4825008ad0bbcc89d664
snehalkadam04 <156996539+snehalkadam04@users.noreply.github.com>
2025-01-26 12:24:31 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" >1);
+console.log("02">1);
+//js convert string into number//output true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);
+//false,false,true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//false,false,false
+
+console.log("2" === 2);
+// avoid the conversion like line  11 to 22
+
+
+
+
+
+
+
