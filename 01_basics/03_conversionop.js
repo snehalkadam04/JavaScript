0bbf13bef09e275c92852016bf8e32614794fdef
snehalkadam04 <156996539+snehalkadam04@users.noreply.github.com>
2025-01-26 09:09:21 +0000

    conversion operation

---
+++ 01_basics/03_conversionop.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) //declation of type
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//if we wrire null insted of 331bc the output will 0
+//if we write undefined insted of 33abc the output will NaN(not a number)
+//if we wrire true/false insted of 33abc the output will 1/0
+//if we wrire string insted of 331bc the output will NaN
+
+
+let isLoggedIn ="snehal k"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+//1 = true, 0 = false
+//"" = false
+//"hitesh"= true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
