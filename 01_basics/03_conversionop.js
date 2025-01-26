1b2fcc13f29bad9d2a962437f386069fd4863f9b
snehalkadam04 <156996539+snehalkadam04@users.noreply.github.com>
2025-01-26 12:07:26 +0000

    conversionoperation

--- 01_basics/03_conversionop.js
+++ 01_basics/03_conversionop.js
@@ -28,3 +28,37 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//*******************************Operation********************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "snehal"
+let str2 = "kadam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+
+console.log(1+2*3);
+
+//le num1 ,num2,num3//
+//num1 =t num2 = num3 = 2+2//
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+//++ add one //output = 101
+
+//prefix and postfix JS,mdn
