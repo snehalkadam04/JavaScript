4d2aa2c052759b5c149476229c81d72c2301e2de
snehalkadam04 <156996539+snehalkadam04@users.noreply.github.com>
2025-02-14 15:56:59 +0000

    memory

---
+++ memory.js
@@ -0,0 +1,25 @@
+//stake memory
+
+let myName = "snehal kadam"
+
+let anotherName = myName
+anotherName = "diksha kadam"
+
+console.log(myName);
+console.log(anotherName);
+
+//heap memory
+
+let userOne = {
+    email: "snehalkadam@google.com"
+}
+
+let usertwo = {
+    email: "dikshakadam@google.com"
+
+}
+
+console.log(userOne);
+console.log(usertwo);
+
+
